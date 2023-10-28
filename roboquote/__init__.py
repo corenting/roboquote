7fcc160f65be1fd550a9697971dfbbcf1f39999f
Corentin Garcia <corenting@gmail.com>
2023-10-28 15:28:45 +0200

    fix: fix init in container

--- roboquote/__init__.py
+++ roboquote/__init__.py
@@ -4,6 +4,8 @@ from loguru import logger
 
 from roboquote import config
 
+__version__ = "0.3.0"
+
 # Setup logger
 logger.remove()
 logger.add(sys.stderr, level=config.LOG_LEVEL)
--- roboquote/web/templates.py
+++ roboquote/web/templates.py
@@ -1,8 +1,7 @@
 """Declare the Jinja2 templates for the web app."""
-from importlib.metadata import version
-
 from starlette.templating import Jinja2Templates
 
+from roboquote import __version__
 from roboquote.constants import DEFAULT_WEB_TEXT_MODEL
 from roboquote.entities.text_model import TextModel
 
@@ -10,7 +9,7 @@ templates = Jinja2Templates(directory="web_templates")
 
 # Add global informations to env
 templates.env.globals["global"] = {
-    "app_version": version("roboquote"),
+    "app_version": __version__,
     "default_text_model_name_for_web_ui": DEFAULT_WEB_TEXT_MODEL.value,
     "available_text_models_names": [i.value for i in TextModel],
 }
